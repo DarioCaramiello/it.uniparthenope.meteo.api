0247f045f08a4f11c9194e9281ce39c41d03d37f
dario <dariocaramiello03@hotmail.com>
2023-07-06 00:19:03 +0200

    configuration

--- mongorestore.sh
+++ mongorestore.sh
@@ -1,1 +1,2 @@
-mongorestore -d ccmma-database --noIndexRestore /backup_db/041823/ccmmma-database
+# mongorestore -d ccmma-database --noIndexRestore /backup_db/041823/ccmmma-database
+mongorestore -d ccmma-database --noIndexRestore /data-db
